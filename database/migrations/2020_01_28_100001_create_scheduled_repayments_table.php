cc56bea7a288746330aa52196279bab1e1ca7fac
supri <supri@vorta.live>
2024-02-21 15:41:34 +0700

    add filed on migration

--- database/migrations/2020_01_28_100001_create_scheduled_repayments_table.php
+++ database/migrations/2020_01_28_100001_create_scheduled_repayments_table.php
@@ -16,9 +16,11 @@ class CreateScheduledRepaymentsTable extends Migration
         Schema::create('scheduled_repayments', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('loan_id');
-
-            // TODO: Add missing columns here
-
+            $table->integer('amount');
+            $table->integer('outstanding_amount')->nullable();
+            $table->string('currency_code');
+            $table->date('due_date');
+            $table->string('status')->nullable();
             $table->timestamps();
             $table->softDeletes();
 
